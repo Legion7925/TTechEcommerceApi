b95504e97407ce5f2a7df0afccfd9180f719796f
amim.7925 <amim.464432@gmail.com>
2023-01-28 12:52:20 +0330

    Add : implemented memory caching for user repository

---
+++ TTechEcommerceApi/Repository/CachedUserService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using TTechEcommerceApi.Interface;
+using TTechEcommerceApi.Model;
+
+namespace TTechEcommerceApi.Repository
+{
+    public class CachedUserService : IUserService
+    {
+        private readonly UserService decorated;
+        private readonly IMemoryCache memoryCache;
+
+        public CachedUserService(UserService decorated, IMemoryCache memoryCache)
+        {
+            this.decorated = decorated;
+            this.memoryCache = memoryCache;
+        }
+
+        public Task<AuthenticateResponseModel> Authenticate(AuthenticateRequestModel model)
+        {
+            return decorated.Authenticate(model);
+        }
+
+        public Task Delete(int userId) => decorated.Delete(userId);
+
+        public IEnumerable<UserResponseModel> GetAllUsers()
+        {
+            var key = "UserList";
+
+             return memoryCache.GetOrCreate(key, entry =>
+             {
+                 entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(2));
+                 entry.SetSlidingExpiration(TimeSpan.FromMinutes(1));
+                 return decorated.GetAllUsers();
+             });
+        }
+
+        public Task<UserResponseModel> Register(UserRequestModel model)
+        {
+            return decorated.Register(model);
+        }
+
+        public Task<UserResponseModel> Update(UserRequestModel model, int userId)
+        {
+            return decorated.Update(model, userId);
+        }
+    }
+}
